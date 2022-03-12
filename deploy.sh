c17cfc3265f9ab135ae8bfb3d275c134a6887acc
AxisRay <rayleesky@outlook.com>
2022-03-12 16:38:07 +0800

    fix cdn refresh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-QINIU_AK=$1
-QINIU_SK=$2
-
-#GetToken
-TOKEN=`echo "/v2/tune/refresh" |openssl dgst -binary -hmac $TOKEN -sha1 |base64 | tr + - | tr / _`
-#Refresh
-curl -X POST -H "Authorization: QBox $QINIU_AK:$token" http://fusion.qiniuapi.com/v2/tune/refresh -d '{"urls":["https://raycn.pub/index.html","https://raycn.pub/archives/index.html"]}' -H 'Content-Type: application/json'
